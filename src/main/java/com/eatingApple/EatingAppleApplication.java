7608692c56d37522cdfc5225bdf05264dd9de934
jiebaosang <mopengdeng@gmail.com>
2022-10-25 10:38:22 +0800

    初始化

---
+++ src/main/java/com/eatingApple/EatingAppleApplication.java
@@ -0,0 +1,13 @@
+package com.eatingApple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingAppleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatingAppleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eatingApple/EatingAppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatingApple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingAppleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
